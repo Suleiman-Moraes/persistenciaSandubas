d2a2fdbc1966c4e2155fccd867c5fb11e9f5a9b5
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-02-05 13:46:36 -0200

    Ajuste para inicializar com tomcat

--- src/main/java/br/com/senaigo/persistenciasandubas/PersistenciaSandubasApplication.java
+++ src/main/java/br/com/senaigo/persistenciasandubas/PersistenciaSandubasApplication.java
@@ -1,12 +1,29 @@
 package br.com.senaigo.persistenciasandubas;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication(scanBasePackages = { "br.com.senaigo.persistenciasandubas" })
-public class PersistenciaSandubasApplication {
+public class PersistenciaSandubasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersistenciaSandubasApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersistenciaSandubasApplication.class);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 }
