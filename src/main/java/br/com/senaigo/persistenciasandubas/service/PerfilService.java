a686a1be407360d824d1f828e2f0a8c31b3f823a
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-02-25 09:12:57 -0300

    atualiza

--- src/main/java/br/com/senaigo/persistenciasandubas/service/PerfilService.java
+++ src/main/java/br/com/senaigo/persistenciasandubas/service/PerfilService.java
@@ -1,11 +1,13 @@
 package br.com.senaigo.persistenciasandubas.service;
 
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
 import br.com.senaigo.persistenciasandubas.model.Perfil;
 import br.com.senaigo.persistenciasandubas.util.CRUDPadraoService;
 import br.com.senaigo.persistenciasandubas.util.FacesUtil;
 
+@Component
 public interface PerfilService extends CRUDPadraoService<Perfil>{
 	public static final String OBJETO_EXISTENTE = FacesUtil.propertiesLoader().getProperty("perfilExistente");
 	
